728ebb5903c1e6604031f59b544363cef4cb8afe
Frantisek Vybiral <fanyvybiral@seznam.cz>
2021-11-29 13:49:59 +0100

    dd

--- main.py
+++ main.py
@@ -2,40 +2,42 @@
 
 from os.path import basename, splitext
 import tkinter as tk
+from tkinter import Label, Button, Scale, HORIZONTAL
 
 # from tkinter import ttk
 
 
-class About(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent, class_=parent.name)
-        self.config()
-
-        btn = tk.Button(self, text="Konec", command=self.close)
-        btn.pack()
-
-    def close(self):
-        self.destroy()
 
 
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
-    name = "Foo"
+    name = "COlor mishmash"
 
     def __init__(self):
         super().__init__(className=self.name)
         self.title(self.name)
         self.bind("<Escape>", self.quit)
-        self.lbl = tk.Label(self, text="Hello World")
-        self.lbl.pack()
+
+        self.lblR = tk.Label(self, text="R")
+        self.lblR.pack()
+        self.scaleR = Scale(from_=0, to=255, orient=HORIZONTAL, length=256)
+        self.scaleR.pack()
+
+        self.lblG = tk.Label(self, text="G")
+        self.lblG.pack()
+        self.scaleG = Scale(from_=0, to=255, orient=HORIZONTAL, length=256)
+        self.scaleG.pack()
+
+        self.lblB = tk.Label(self, text="B")
+        self.lblB.pack()
+        self.scaleB = Scale(from_=0, to=255, orient=HORIZONTAL, length=256)
+        self.scaleB.pack()
+
         self.btn = tk.Button(self, text="Quit", command=self.quit)
         self.btn.pack()
-        self.btn2 = tk.Button(self, text="About", command=self.about)
-        self.btn2.pack()
 
-    def about(self):
-        window = About(self)
-        window.grab_set()
+        
+
 
     def quit(self, event=None):
         super().quit()
